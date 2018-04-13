b4446854753b8df4735304239ebbffe7d7efe66e
Aleksander Matyjewicz <matyjewiczaleksander@gmail.com>
2018-04-13 12:16:19 +0200

    Zadanie_14_3-DONE!

--- script.js
+++ script.js
@@ -3,29 +3,33 @@ var movies = [
 		id: 1,
 		title: 'Harry Potter',
 		dsc: 'Film o czarodzieju',
-		img: './kiler.png'
+		img: './Harry-Potter.jpg'
 	},
 	{   
 		id: 2,
 		title: 'Psy 2',
-		dsc: 'Film o Bogusiu Lindzie'
+		dsc: 'Film o Bogusiu Lindzie',
+		img: './Psy-2.jpg'
 	},
 	{   
 		id: 3,
 		title: 'Nietykalni',
-		dsc: 'Film o murzynie'
+		dsc: 'Film o murzynie',
+		img: './nietykalni-10.png'
 	},
 	{   
 		id: 4,
 		title: 'Killer',
 		dsc: 'Czarek pzura w roli głownej',
+		img: './kiler.png'
 	}
 ];
 	
 var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
       React.createElement('h2', {}, movie.title),
-      React.createElement('p', {}, movie.dsc)
+      React.createElement('p', {}, movie.dsc),
+      React.createElement('img', {src: movie.img})
     );
 });
 
